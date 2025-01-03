c9e7d69841b011f8fd058891578f86f84f924d79
drahcirok <richardbp2006@gmail.com>
2025-01-03 10:30:41 -0500

    Update app.module.ts
    firebase Modulo

--- novacontactbackend/src/app.module.ts
+++ novacontactbackend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FirebaseModule } from './firebase/firebase.module'; // Importa el módulo de Firebase
 
 @Module({
-  imports: [],
+  imports: [FirebaseModule], // Asegúrate de que FirebaseModule esté aquí
   controllers: [AppController],
   providers: [AppService],
 })
